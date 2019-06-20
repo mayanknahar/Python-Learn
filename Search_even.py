9c86541c1bfc5e995c1bfab146f115a3b83cd4de
mayanknahar <mayanknahar97@gmail.com>
2019-06-20 13:50:39 +0530

    Second Commit

---
+++ Search_even.py
@@ -0,0 +1,7 @@
+numbers=[11,12,33,44,43,55,75,87,43]
+for num in numbers:
+	if num%2==0:
+		print('the list contains an even number',num)
+		
+else:
+	print('the list doesnt contain even no')
---
+++ multiplication.py
@@ -0,0 +1,6 @@
+import sys
+for i in range(1,11):
+	for j in range(1,11): 
+		k=i*j
+		print(k,end=' ')
+	print()
